8dd352c507a82fa2502d3d2739d050e190b02031
Majid <Mansouridb@gmail.com>
2021-04-22 09:56:16 +0200

    Initial commit

---
+++ src/main/java/org/example/SplitOW.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SplitOW {
+    public static void main(String[] args) {
+String str="Oil Water" ;
+String [ ] w =str.split(" ");
+        System.out.println(Arrays.toString(w));
+
+
+    }
+}
